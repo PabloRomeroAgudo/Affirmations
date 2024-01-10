5ab3bf69fcfac0ad36b9584e777c613116c1c8f8
PabloRomeroAgudo <pabloromeroagudo@gmail.com>
2024-01-10 13:09:16 +0100

    added icon to app

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AffirmationsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
